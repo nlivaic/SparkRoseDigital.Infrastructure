bd954acf96bb0ab59a3dbdf9f845283e8ebcf9e0
Nenad Livaic <nenad.livaic@cz.pwc.com>
2022-08-14 14:26:09 +0200

    dev: Remove MassTransit.

--- src/SparkRoseDigital.Infrastructure/MessageBroker/IEventRegister.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SparkRoseDigital.Infrastructure.MessageBroker
-{
-    public interface IEventRegister
-    {
-        void RegisterEvent<T>(object newEvent)
-            where T : class;
-    }
-}
