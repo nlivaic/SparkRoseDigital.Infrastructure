5e6d15c8f08766adab6687ef1ddd4d42052ebab0
Nenad Livaic <nenad.livaic@cz.pwc.com>
2023-01-19 10:27:22 +0100

    Application Insights. Removed Seq. [+semver: minor]

---
+++ src/SparkRoseDigital.Infrastructure/Logging/SerilogConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace SparkRoseDigital.Infrastructure.Logging
+{
+    public static class SerilogConfiguration
+    {
+        public static Action<HostBuilderContext, IServiceProvider, LoggerConfiguration> ConfigureLogger =
+            (HostBuilderContext context, IServiceProvider services, LoggerConfiguration loggerConfiguration) =>
+            loggerConfiguration
+                .WriteTo.ApplicationInsights(
+                    services.GetRequiredService<TelemetryConfiguration>(),
+            TelemetryConverter.Traces);
+    }
+}
