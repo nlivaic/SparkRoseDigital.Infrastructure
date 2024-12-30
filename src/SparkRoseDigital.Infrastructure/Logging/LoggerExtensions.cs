bdb6d3b1cc40fcc5aa467b1fd1c8cc0b797644cd
Nenad Livaic <nenad@revelator.com>
2024-12-30 12:34:15 +0100

    Refactor activity logging method.

--- src/SparkRoseDigital.Infrastructure/Logging/LoggerExtensions.cs
+++ src/SparkRoseDigital.Infrastructure/Logging/LoggerExtensions.cs
@@ -35,16 +35,14 @@ namespace SparkRoseDigital.Infrastructure.Logging
         }
 
         /// <summary>
-        /// Adds a listener so every new Activity is logged.
-        /// Makes sure the tracing is W3C Trace Context compliant.
+        /// Use for debugging! Adds a listener so every new Activity is logged.
         /// Makes tracing easier due to some components (e.g. HttpClient, MassTransit)
         /// create their own Activity and the attached listener allows end-to-end tracing.
         /// </summary>
         /// <param name="host">Host.</param>
         /// <returns>Host with activity logging configured.</returns>
-        public static IHost AddW3CTraceContextActivityLogging(this IHost host)
+        public static IHost AddActivityLogging(this IHost host)
         {
-            Activity.DefaultIdFormat = ActivityIdFormat.W3C;
             using (var scope = host.Services.CreateScope())
             {
                 var logger = scope.ServiceProvider.GetRequiredService<ILogger<Activity>>();
@@ -53,11 +51,11 @@ namespace SparkRoseDigital.Infrastructure.Logging
                     ShouldListenTo = _ => true,
                     ActivityStarted = activity =>
                     {
-                        logger.LogInformation("Activity started: {operationName}.", activity.OperationName);
+                        logger.LogTrace("Activity started: {operationName}.", activity.OperationName);
                     },
                     ActivityStopped = activity =>
                     {
-                        logger.LogInformation("Activity stopped: {operationName}.", activity.OperationName);
+                        logger.LogTrace("Activity stopped: {operationName}.", activity.OperationName);
                     }
                 });
             }
